c687b3621ddcf94f39469a88d7a188286d67ca25
Liviu Nicoara <nikkoara at hates dot ms>
2017-07-22 09:54:56 -0400

    CSLDP operator.

---
+++ include/lbp/csldp.hpp
@@ -0,0 +1,73 @@
+#ifndef LBP_CSLDP_HPP
+#define LBP_CSLDP_HPP
+
+#include <lbp/defs.hpp>
+#include <lbp/utils.hpp>
+#include <lbp/detail/neighborhoods.hpp>
+#include <lbp/detail/sampling.hpp>
+
+#include <opencv2/core.hpp>
+
+#include <boost/hana/fold.hpp>
+#include <boost/hana/tuple.hpp>
+
+#include <boost/integer.hpp>
+
+//
+// @inproceedings{xue2011hybrid,
+//   title={Hybrid center-symmetric local pattern for dynamic background subtraction},
+//   author={Xue, Gengjian and Song, Li and Sun, Jun and Wu, Meng},
+//   booktitle={Multimedia and Expo (ICME), 2011 IEEE International Conference on},
+//   pages={1--6},
+//   year={2011},
+//   organization={IEEE}
+//}
+//
+
+namespace lbp {
+namespace csldp_detail {
+
+template< typename T >
+auto csldp = [](auto neighborhood, auto sampler) {
+    return [=](const cv::Mat& src, size_t i, size_t j) {
+        namespace hana = boost::hana;
+        using namespace hana::literals;
+
+        const auto c = src.at< T > (i, j);
+
+        return hana::fold (
+            neighborhood, 0, [&, S = 0](auto accum, auto x) mutable {
+                const auto a = sampler (src, i + x [0_c], j + x [1_c]);
+                const auto b = sampler (src, i - x [0_c], j - x [1_c]);
+                return accum | (((a >= c) ^ (b >= c)) << S++);
+            });
+    };
+};
+
+} // namespace csldp_detail
+
+template< typename T, size_t R, size_t P >
+auto csldp = [](const cv::Mat& src) {
+    using value_type = typename boost::uint_t< P >::least;
+
+    cv::Mat dst (
+        src.size (), opencv_type< (sizeof (value_type) << 3) >,
+        cv::Scalar (0));
+
+    auto op = csldp_detail::csldp< T > (
+        detail::semicircular_neighborhood< R, P >,
+        detail::nearest_sampler< T >);
+
+#pragma omp parallel for
+    for (size_t i = R; i < src.rows - R; ++i) {
+        for (size_t j = R; j < src.cols - R; ++j) {
+            dst.at< unsigned char > (i, j) = op (src, i, j);
+        }
+    }
+
+    return dst;
+};
+
+} // namespace lbp
+
+#endif // LBP_CSLDP_HPP
